c400149ef5a864b8be5f85743622dd123f8246ee
marcvinius <marcosviniciuscpanta@gmail.com>
2024-09-30 12:26:31 -0300

    alt1

---
+++ Intro_a_progamação/Aula_5/Ted5_1.py
@@ -0,0 +1,9 @@
+# Solicite os valores de X e Y do usuário
+x = int(input("Digite o número de passos para o norte (X): "))
+y = int(input("Digite o número de passos para o leste (Y): "))
+
+# Calcule a distância total
+distancia_total = x + y
+
+# Exibir o resultado
+print(f"A distância total que o pirata precisa percorrer é: {distancia_total} passos")
